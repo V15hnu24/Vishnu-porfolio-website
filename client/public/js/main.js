242e8972021fff7db46962ead88b444a2ddf8d93
V15hnu24 <799vishnu530@gmail.com>
2022-07-24 00:25:57 +0530

    Getform api Endpoint and python smtp

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -35,29 +35,29 @@ function readmore_function(part){
         moretext.style.display = "none";
     }
 }
-// const form = document.querySelector('form');
-// const submit_btn = document.getElementById("submit-btn");
-// const thankyou_msg = document.getElementById("thankyou-msg");
-// const name = document.getElementById("form-name");
-// const phone = document.getElementById("form-phone");
-// const email = document.getElementById("form-email");
-// const message = document.getElementById("message_contact_wrapper");
+const form = document.querySelector('form');
+const submit_btn = document.getElementById("submit-btn");
+const thankyou_msg = document.getElementById("thankyou-msg");
+const name = document.getElementById("form-name");
+const phone = document.getElementById("form-phone");
+const email = document.getElementById("form-email");
+const message = document.getElementById("message_contact_wrapper");
 
-// // submit_btn.addEventListener("click", (e) =>{
-// //     console.log(name.value);
-// //     console.log(phone.value);
-// //     e.preventDefault();
-// //     thankyou_msg.classList.add('show');
-// //     // name.value = "";
-// //     // phone.value = "";
-// //     // email.value = "";
-// //     // message.value = "";
-// // });
-// form.addEventListener('submit', (e) =>{
+// submit_btn.addEventListener("click", (e) =>{
 //     console.log(name.value);
 //     console.log(phone.value);
+//     e.preventDefault();
 //     thankyou_msg.classList.add('show');
+//     // name.value = "";
+//     // phone.value = "";
+//     // email.value = "";
+//     // message.value = "";
 // });
+form.addEventListener('submit', (e) =>{
+    console.log(name.value);
+    console.log(phone.value);
+    thankyou_msg.classList.add('show');
+});
 
 // $(function() {
 //     $( "#submit-button" ).click(function() {
@@ -90,6 +90,31 @@ function readmore_function(part){
 //         })
 //     })
 // }
+
+function submit_action(){
+    setTimeout(colorChangeer,1000);
+    setTimeout(colorChangeertoGreen,2000);
+    setTimeout(reset, 3000);
+};
+function colorChangeer(){
+    console.log("From temp");
+    const btn = document.getElementById('Submit');
+    btn.style.backgroundColor = "purple";
+    btn.innerText = 'Sending...';
+};
+function colorChangeertoGreen(){
+    console.log("From temp");
+    const btn = document.getElementById('Submit');
+    btn.style.backgroundColor = "Green";
+    btn.innerText = 'Sent';
+};
+function reset(){
+    const form = document.getElementById('Contactform');
+    form.reset();
+    const btn = document.getElementById('Submit');
+    btn.style.backgroundColor = "transparent";
+    btn.innerText = 'Submit';
+};
 
 // let c = 1;
 // let temp1 = document.querySelector(".navbar-toggler");;
